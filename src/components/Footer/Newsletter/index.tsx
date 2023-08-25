73abacc08a23b978f371535a229bfecdf9e4682f
denbarabraza <denis.bareischev@gmail.com>
2023-08-25 11:03:43 +0300

    feat: added emailjs in footer

--- src/components/Footer/Newsletter/index.tsx
+++ src/components/Footer/Newsletter/index.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslations } from 'next-intl';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
+import { envEmailJs } from '@/constants/envEmailJs';
 import { newsLetterFooterSchema } from '@/utils/validation';
+import emailjs from '@emailjs/browser';
 import { yupResolver } from '@hookform/resolvers/yup';
 
 import { INewsLetterFooterInput } from './interface';
@@ -12,8 +14,10 @@ import { INewsLetterFooterInput } from './interface';
 import styles from './styles.module.scss';
 
 const Newsletter = () => {
+  const formRef = useRef<HTMLFormElement>(null);
   const t = useTranslations();
   const [alertText, setAlertText] = useState<string>('');
+  let timerId: ReturnType<typeof setTimeout> | undefined;
 
   const {
     register,
@@ -25,26 +29,58 @@ const Newsletter = () => {
     mode: 'onTouched',
   });
 
-  const handleSubscribe = async ({ email }: INewsLetterFooterInput) => {
-    console.log(email);
+  const {
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+    NEXT_PUBLIC_EMAILJS_NEWSLETTER_TEMPLATE,
+  } = envEmailJs;
+
+  const handleSubscribe: SubmitHandler<INewsLetterFooterInput> = () => {
+    console.log(formRef.current);
+    emailjs
+      .sendForm(
+        NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        NEXT_PUBLIC_EMAILJS_NEWSLETTER_TEMPLATE,
+        formRef.current as HTMLFormElement,
+        NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+      )
+      .then(
+        result => {
+          if (result.text === 'OK') {
+            setAlertText(t('Footer.subscribedAlert'));
+            timerId = setTimeout(() => {
+              setAlertText('');
+            }, 5000);
+          }
+        },
+        error => {
+          setAlertText(error.text);
+          timerId = setTimeout(() => {
+            setAlertText('');
+          }, 5000);
+        },
+      );
     reset();
   };
 
-  const handleCloseAlert = () => {
-    setAlertText('');
-  };
+  useEffect(() => {
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
+  }, [timerId]);
 
   return (
-    <form className={styles.wrapper} onSubmit={handleSubmit(handleSubscribe)}>
+    <form
+      className={styles.wrapper}
+      onSubmit={handleSubmit(handleSubscribe)}
+      ref={formRef}
+    >
       <div className={styles.form}>
         <h3 className={styles.title}>{t('Newsletter.title')}</h3>
         <section className={styles.main}>
           <div className={styles.inputWrapper}>
-            {alertText && (
-              <button type='button' className={styles.alert} onClick={handleCloseAlert}>
-                {alertText}
-              </button>
-            )}
             <input
               type='email'
               {...register('email', {
@@ -58,6 +94,7 @@ const Newsletter = () => {
           <button className={styles.button} type='submit'>
             {t('Newsletter.button')}
           </button>
+          {alertText && <div className={styles.alert}>{alertText}</div>}
         </section>
       </div>
     </form>
